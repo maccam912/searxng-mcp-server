3df9cae037bf5810955532f836384fc26ad638a2
Matt Koski <maccam912@gmail.com>
2025-01-13 22:30:43 -0600

    take 2

--- searxng_mcp_server/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Matt Koski <maccam912@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.2'
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Matt Koski <maccam912@gmail.com>
-#
-# SPDX-License-Identifier: MIT
